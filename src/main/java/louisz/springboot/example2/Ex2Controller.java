8096445954778e99e2817bb1a1468f1b3a148aae
louisz6ster <louisz6ster@gmail.com>
2017-06-18 22:20:44 +0800

    增加log and banner

--- src/main/java/louisz/springboot/example2/Ex2Controller.java
+++ src/main/java/louisz/springboot/example2/Ex2Controller.java
@@ -1,12 +1,31 @@
 package louisz.springboot.example2;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Ex2Controller {
-	@RequestMapping("/")
-    public String index() {
-        return "This is Louisz Spring Boot!";
+	//請注意不要用static，才能使用this.getClass
+	 private final Logger logger = LoggerFactory.getLogger(this.getClass()); 
+	
+	@RequestMapping("/log_debug")
+    public String debug() {
+		logger.debug("This is debug msg.");		
+        return "debug level";
     } 
+	
+	@RequestMapping("/log_info")
+    public String info() {
+		logger.info("This is info msg.");		
+        return "info level";
+    } 
+	@RequestMapping("/log_error")
+    public String error() {
+		logger.debug("This is error msg.");		
+        return "error level";
+    } 
+	
+	
 }
