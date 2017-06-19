710fed461866347fc1a59777402a72a447b4a0f0
louisz6ster <louisz6ster@gmail.com>
2017-06-19 22:36:42 +0800

    增加logback設定檔及banner

--- src/main/java/louisz/springboot/example2/Ex2Controller.java
+++ src/main/java/louisz/springboot/example2/Ex2Controller.java
@@ -1,5 +1,6 @@
 package louisz.springboot.example2;
 
+//採用的是slf4j外框
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,17 +11,19 @@ public class Ex2Controller {
 	//請注意不要用static，才能使用this.getClass
 	 private final Logger logger = LoggerFactory.getLogger(this.getClass()); 
 	
+	 //log level 設定為debug
 	@RequestMapping("/log_debug")
     public String debug() {
 		logger.debug("This is debug msg.");		
         return "debug level";
     } 
-	
+	 //log level 設定為info
 	@RequestMapping("/log_info")
     public String info() {
 		logger.info("This is info msg.");		
         return "info level";
     } 
+	 //log level 設定為error
 	@RequestMapping("/log_error")
     public String error() {
 		logger.debug("This is error msg.");		
--- src/main/java/louisz/springboot/example2/Example2.java
+++ src/main/java/louisz/springboot/example2/Example2.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Louisz Spring boot introduce ex.1
+ * Louisz Spring boot introduce ex.2
  *
  */
 
