e4a82174723916111ff9388a97b4ceb089c9fe39
louisz6ster <louisz6ster@gmail.com>
2017-06-29 23:25:40 +0800

    example 4 finish

--- src/main/java/louisz/springboot/example4/ExampleProperties.java
+++ src/main/java/louisz/springboot/example4/ExampleProperties.java
@@ -4,14 +4,24 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
-@Component
-@PropertySource("classpath:example4.properties")
-@ConfigurationProperties("app")
+@Component // DI之後介紹
+@PropertySource("classpath:example4.properties") //設定自訂的設定檔檔名及路徑
+@ConfigurationProperties("app") //prefix
 public class ExampleProperties {
-	
+	//@Value("${app.name}") 也可以採用這種方式，這樣name可以不用相同，但是不建議
+	//，因為後續不好維護
 	private String name;
 	
 	private String version;
+	
+	private String nameversion;
+	
+	private String stringvalue;
+	private int intvalue;
+	private long longvalue;
+	private int tenvalue;
+	private int twentyvalue;
+	
 	
 	public String getName() {
 		return name;
@@ -24,6 +34,42 @@ public class ExampleProperties {
 	}
 	public void setVersion(String version) {
 		this.version = version;
+	}
+	public String getNameversion() {
+		return nameversion;
+	}
+	public void setNameversion(String nameversion) {
+		this.nameversion = nameversion;
+	}
+	public String getStringvalue() {
+		return stringvalue;
+	}
+	public void setStringvalue(String stringvalue) {
+		this.stringvalue = stringvalue;
+	}
+	public int getIntvalue() {
+		return intvalue;
+	}
+	public void setIntvalue(int intvalue) {
+		this.intvalue = intvalue;
+	}
+	public long getLongvalue() {
+		return longvalue;
+	}
+	public void setLongvalue(long longvalue) {
+		this.longvalue = longvalue;
+	}
+	public int getTenvalue() {
+		return tenvalue;
+	}
+	public void setTenvalue(int tenvalue) {
+		this.tenvalue = tenvalue;
+	}
+	public int getTwentyvalue() {
+		return twentyvalue;
+	}
+	public void setTwentyvalue(int twentyvalue) {
+		this.twentyvalue = twentyvalue;
 	}
 
 	
--- src/test/java/louisz/springboot/example4/TestExampleProperties.java
+++ src/test/java/louisz/springboot/example4/TestExampleProperties.java
@@ -14,12 +14,16 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 public class TestExampleProperties {
 	@Autowired
-	private ExampleProperties exampleProperties;
+	private ExampleProperties exampleProperties;//注入
 	
 	@Test
 	public void getHello() throws Exception {
+		//驗證是否符合預期
 		Assert.assertEquals(exampleProperties.getName(), "example4");
 		Assert.assertEquals(exampleProperties.getVersion(), "1");
+		Assert.assertEquals(exampleProperties.getNameversion(), "example4_1");
+		System.out.println(exampleProperties.getIntvalue());
+		System.out.println(exampleProperties.getTwentyvalue());
 	}
 	
 }
